ce56824ea3fffb0b784c3caa9fed437e2283cfdc
satoshionoue <satoshionoue06@gmail.com>
2025-05-29 16:21:58 +0900

    modified:   est_dir.sh

--- est_dir.sh
+++ est_dir.sh
@@ -1,6 +1,6 @@
 #各イテレーションに対して実行して評価する
 WORKSPACE=$(pwd)
-ITERATION=0
+ITERATION=20000
 while [ $ITERATION -le 300000 ]; do
     echo "Iteration: $ITERATION"
     python3 pytorch/calculate_score_for_paper.py infer_prob --workspace='.' --model_type='Regress_onset_offset_frame_velocity_CRNN' --checkpoint_path="ckpts/${ITERATION}_iterations.pth" --augmentation='none' --dataset='maestro' --split='test' --cuda
