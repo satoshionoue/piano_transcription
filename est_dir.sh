158bbf78f11f8b0a466befe4e101d3db4d58e488
satoshionoue <satoshionoue06@gmail.com>
2025-07-11 12:49:32 +0900

    Update .gitignore, est_dir.sh, runme.sh, and add new scripts for inference and evaluation

--- est_dir.sh
+++ est_dir.sh
@@ -1,23 +1,13 @@
-#各イテレーションに対して実行して評価する
-# WORKSPACE=$(pwd)
-# ITERATION=20000
-# while [ $ITERATION -le 300000 ]; do
-#     echo "Iteration: $ITERATION"
-#     python3 pytorch/calculate_score_for_paper.py infer_prob --workspace='.' --model_type='Regress_onset_offset_frame_velocity_CRNN' --checkpoint_path="ckpts/${ITERATION}_iterations.pth" --augmentation='none' --dataset='maestro' --split='test' --cuda
-#     ITERATION=$((ITERATION + 20000))
-# done
+#!/bin/bash
 
-# WORKSPACE=$(pwd)
-# ITERATION=0
-# while [ $ITERATION -le 300000 ]; do
-#     echo "Iteration: $ITERATION"
-#     python3 pytorch/calculate_score_for_paper.py calculate_metrics --workspace=$WORKSPACE --model_type='Regress_onset_offset_frame_velocity_CRNN' --augmentation='aug' --dataset='maestro' --split='test' --checkpoint_path="ckpts/${ITERATION}_iterations.pth"
-#     ITERATION=$((ITERATION + 20000))
-# done
+# wavファイルがあるディレクトリを引数で受け取る
+if [ $# -ne 1 ]; then
+    echo "使い方: $0 <wavファイルのディレクトリ>"
+    exit 1
+fi
 
-WORKSPACE=$(pwd)
-for ITERATION in 0 20000 100000 140000 160000 200000 240000
-do
-    echo "Iteration: $ITERATION"
-    python3 pytorch/calculate_score_for_paper.py calculate_metrics --workspace=$WORKSPACE --model_type='Regress_onset_offset_frame_velocity_CRNN' --augmentation='aug' --dataset='maestro' --split='test' --checkpoint_path="ckpts/${ITERATION}_iterations.pth"
+wav_dir="$1"
+
+for wav_file in "$wav_dir"/*.wav; do
+    python3 pytorch/inference.py --model_type='Regress_onset_offset_frame_velocity_CRNN' --checkpoint_path="17000_iterations.pth" --audio_path="$wav_file" --cuda
 done
